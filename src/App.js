efeee51ba3ae5002fd62abf67d707c3d9334c46a
Aalekh42 <aalekhpradeep@gmail.com>
2021-05-31 13:14:04 +0530

    7th commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Router>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/Home">
+            <div>Chala JAA </div>
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
